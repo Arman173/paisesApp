9d59d5a1ef66cf83b9bcb725c97d499629a1a95e
Arman173 <armanhaz173@gmail.com>
2022-01-12 21:46:04 -0500

    verPais added

--- src/app/paises/pages/ver-pais/ver-pais.component.ts
+++ src/app/paises/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
+
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -8,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  pais!: Country;
+
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _paisService: PaisService
+  ) { }
 
   ngOnInit(): void {
+
+    this._activatedRoute.params
+      .pipe(
+        // switchMap nos permite retornar otro Observable
+        switchMap( ({ id }) => this._paisService.getPaisPorAlpha( id ) ),
+        tap( console.log )
+      )
+      .subscribe( pais => this.pais = pais );
+
+    // this._activatedRoute.params
+    //   .subscribe( ({ id }) => {
+    //     console.log( id );
+
+    //     this._paisService.getPaisPorAlpha( id )
+    //       .subscribe( pais => {
+    //         console.log( pais );
+    //       });
+
+    //   });
   }
 
 }
--- src/app/paises/services/pais.service.ts
+++ src/app/paises/services/pais.service.ts
@@ -26,4 +26,10 @@ export class PaisService {
     return this._http.get<Country[]>( url );
   }
 
+  getPaisPorAlpha( id: string ): Observable<Country> {
+
+    const url = `${ this.apiUrl }/alpha/${ id }`;
+    return this._http.get<Country>( url );
+  }
+
 }
